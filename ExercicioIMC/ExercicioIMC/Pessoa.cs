4fa2a0c3d1325bc82d94aa9cc9b2044f9d3348e4
GABRIEL VASCONCELOS VALE <gabriel.vale@alunos.sc.senac.br>
2022-07-21 12:13:33 -0300

    exercicio

---
+++ ExercicioIMC/ExercicioIMC/Pessoa.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioIMC
+{
+    public class Pessoa
+    {
+
+
+        public Double peso, altura;
+        
+        public double Calcular()
+        {
+            return peso / (altura*altura);
+
+        }
+
+        public string situacao(double imc)
+        {
+            string retorno;
+
+            if(imc < 18.5)
+            {
+                retorno = "Abaixo do peso";
+            }
+            else if(imc < 25 && imc > 18.5)
+            {
+                retorno = "Peso normal";
+            }
+            else if (imc < 30 && imc > 25)
+            {
+                retorno = "Acima do peso";
+            }
+            else if (imc < 35 && imc > 30)
+            {
+                retorno = "Obesidade I";
+            }
+            else if (imc < 40 && imc > 35)
+            {
+                retorno = "Obesidade II";
+            }
+            else
+            {
+                retorno = "Obesidade III";
+            }
+            return retorno;
+        }
+
+        public void Mensagem()
+        {
+
+            double obterCalculo = Calcular();
+
+            string obterSituacao = situacao(obterCalculo);
+
+            Console.WriteLine("Seu IMC é " + obterCalculo);
+            Console.WriteLine("Sua situação é " + obterSituacao);
+
+
+        }
+
+    }
+}
---
+++ ExercicioIMC/ExercicioIMC/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ExercicioIMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p = new Pessoa();
+            p.peso = 73;
+            p.altura = 1.79;
+            p.Mensagem();
+        }
+    }
+}
